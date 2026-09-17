1d54b8f5f55a3afbd523142715abbd176f35b613
gg <lisboavinicius344@gmail.com>
2026-09-17 15:44:45 -0300

    menu.py

---
+++ setempro.py
@@ -0,0 +1,68 @@
+while True:
+
+    print ("Administração pública\n")
+
+    print ("Escolha opção")
+
+    print ("1 - Cadastrar")
+
+    print ("2 - Exibir")
+
+    print("3 - Editar")
+
+    print("4 - Deletar")
+
+    print("5 - Pesquisar")
+
+    print("6 - Gerar relatorio")
+
+    print("0 - Sair")
+
+
+
+    opcao = input("Insira opção: ")
+
+    if opcao == "1":
+        print("parte do cadastro")
+
+##parte d qm for fazer o cadastro
+
+    elif opcao == "2":
+        print("parte de exibir")
+
+##parte d qm for fazer o exibir
+
+    elif opcao == "3":
+        print("parte de editar")
+
+##parte d qm for fazer a edicao
+    elif opcao == "4":
+        print("parte de deletar")
+
+##parte d qm for fazer o delete
+    elif opcao == "5":
+        print("parte de pesquisa")
+
+##parte d qm for fazer a pesquisa
+
+
+
+    elif opcao == "6":
+
+        print("parte de gerar o relatorio")
+
+##parte d qm for fazer o relatorio
+
+
+
+    elif opcao == "0":
+
+        print("parte de sair")
+
+        break
+
+    else:
+
+         print("opcao invalido")
+
+
