43226de65c579748d14ba3c78d720892b4b6585c
Mirellaarj <mirella.tec2025@gmail.com>
2026-09-24 20:13:21 -0300

    editaredeletar

--- administracaoPublica.py
+++ administracaoPublica.py
@@ -92,34 +92,34 @@ while True:
       arq = open('dados.json', 'r', encoding='utf-8')
       dados = json.load(arq)
       arq.close()
-
-      print("editar servidor")
-
-      id_editar = input("Digite o ID do servidor: ")
-
-      for servidor in dados:
-       if str(servidor['id']) == id_editar:
-
-        print("Nome atual:", servidor['nome'])
-        print("Cargo atual:", servidor['cargo'])
-        print("Órgão atual:", servidor['orgao'])
-        print("Matrícula atual:", servidor['matricula'])
-
-        servidor['nome'] = input("Novo nome: ")
-        servidor['cargo'] = input("Novo cargo: ")
-        servidor['orgao'] = input("Novo órgão: ")
-        servidor['matricula'] = input("Nova matrícula: ")
-    arq = open('dados.json', 'w', encoding='utf-8')
-    json.dump(dados, arq, ensure_ascii=False, indent=4)
-    arq.close()
-
-    print("Servidor editado")
-
-    break
-
+            
+    print('\n Editar servidor publico')
+    id_editar = input('Digite o ID do servidor: ')
+            
+    encontrado = False 
+            
+    for servidor in dados:
+                if str(servidor['id']) == id_editar:
+                    print('Nome atual:', servidor['nome'])
+                    print('Cargo atual:', servidor['cargo'])
+                    print('Órgão atual:', servidor['orgao'])
+                    print('Matrícula atual:', servidor['matrícula'])
+                    
+                    servidor['nome'] = input('Novo nome: ')
+                    servidor['cargo'] = input('Novo cargo: ')
+                    servidor['orgao'] = input('Novo órgão: ')
+                    servidor['matrícula'] = input('Nova matrícula: ')
+                    
+                    arq = open('dados.json', 'w', encoding='utf-8')
+                    json.dump(dados, arq, ensure_ascii=False, indent=4)
+                    arq.close()
+                    print('Servidor editado com sucesso.')
+                    
+                    encontrado = True
+                    break
+                    
     if encontrado == False:
-            print("ID não encontrado")
-
+                print('ID não encontrado.')
     elif opcao == "4":
      print("parte de deletar")
 
